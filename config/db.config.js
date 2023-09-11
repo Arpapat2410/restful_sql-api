d86588ae8a8915ab3fb3b1ec391e092e83c93353
Arpapat2410 <120714445+Arpapat2410@users.noreply.github.com>
2023-09-11 09:54:18 +0700

    change configs

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 
+console.log(process.env.HOST,process.env.PASSWORD,process.env.USER);
+
 module.exports = {
-    HOST :"process.env.HOST",
-    USER :"process.env.USER",
-    PASSWORD : "process.env.PASSWORD",
-    DB : "process.env.DB"
+    HOST :process.env.HOST,
+    USER :process.env.USER,
+    PASSWORD : process.env.PASSWORD,
+    DB : process.env.DB
 }
