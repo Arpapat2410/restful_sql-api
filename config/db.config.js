115f310f6716aab48fb120a29739195e655d7d90
Arpapat2410 <120714445+Arpapat2410@users.noreply.github.com>
2023-09-11 09:37:26 +0700

    config

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+
 module.exports = {
-    HOST :"aws.connect.psdb.cloud",
-    USER :"hjitsyeu39sdojdccm7i",
-    PASSWORD : "pscale_pw_nuT8JeBl9PCcuMnJD00Jbfo4GtVyhPCXtpHc9Te0npZ",
-    DB : "se_database"
+    HOST :"process.env.HOST",
+    USER :"process.env.USER",
+    PASSWORD : "process.env.PASSWORD",
+    DB : "process.env.DB"
 }
